6bdd8bc6b9b651552de74d73c50d8b49b6b1a8fe
Daniyar <dan_ntu@mail.ru>
2016-11-24 12:47:31 +0600

    Add files via upload

--- Box.java
+++ Box.java
@@ -8,68 +8,31 @@
  *
  * @author DMyrzaka
  */
-public class Box {
-    double w;
-    double h;
-    double d;
-    
-    Box() {
-        System.out.println("Constructor Box");
-        w=h=d=10;
-       
-    }
-    
-    Box(double w, double h, double d)
-    {
-        w=w;
-        h=h;
-        d=d;
-        
-    }
-    
-    Box(double w, double h)
-    {
-        w=w;
-        h=h;
-             
-    }
-    
-    Box(double w)
-    {
-        w=w;
-        
-        
-    }
-    
-    
-    
-    
-    double volume (){
-      
-       //double s;
-        //System.out.println(width*height*depth);
-         return this.w*this.h*this.d;
-    }
-    
-    double volume1() {
-        w=5;
-        h=2;
-        d=2;
-        
-        return volume();
-    }
-    
-    void SetDim(double w, double h, double d)
-    {
-        this.w=w;
-        this.h=h;
-        this.d=d;
-        
-    }
-    
-    
-    
-    
-    
-}
+class Box {
+    double width;
+    double height;
+    double depth;
 
+    Box(Box ob) {
+     width=ob.width;
+     height=ob.height;
+     depth=ob.depth;
+    }
+
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+    
+    Box(){
+        width=height=depth=-1;
+    }
+    Box(double len){
+        width=height=depth=len;
+    }
+    
+    double volum(){
+        return width*height*depth;            
+    }      
+}
---
+++ SimpleInheritance.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DMyrzaka
+ */
+class A{
+    int i,j;
+    void showij(){
+        System.out.println("i and j: "+i+" "+j);
+    }
+}
+
+class B extends A{
+    int k;
+    void showk(){
+        System.out.println("k: "+k);  
+    }
+    
+    void sum(){
+        System.out.println("i+j+k: "+(i+j+k));
+    }
+}
+public class SimpleInheritance {
+    public static void main(String args[]){
+        A superOb = new A();
+        B subOb = new B();
+        
+        superOb.i=10;
+        superOb.j=20;
+        System.out.println("Content superOb: ");
+        superOb.showij();
+        System.out.println();
+        
+        subOb.i=7;
+        subOb.j=8;
+        subOb.k=9;
+        System.out.println("Content subOb: ");
+        subOb.showij();
+        subOb.showk();
+        
+        System.out.println();
+        System.out.println("Sum i,j and k in subOb:");
+        subOb.sum();
+    }
+}
