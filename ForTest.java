9b06825fc92d158782227451e70b9315da3f2922
Daniyar <dan_ntu@mail.ru>
2016-11-24 12:45:30 +0600

    Add files via upload

---
+++ ForTest.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DMyrzaka
+ */
+public class ForTest {
+    
+    //@SuppressWarnings("empty-statement")
+    public static void main(String [] args) {
+        
+       int a=9;
+       
+       a=a<<2;
+        System.out.println("a="+a);
+        
+        
+    
+    }
+   
+    
+}
