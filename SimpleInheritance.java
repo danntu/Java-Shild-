65c3c40c60d32394f8712e9aa2ca86adc578e7f5
Daniyar <dan_ntu@mail.ru>
2016-12-08 17:30:48 +0600

    Sanya version

--- SimpleInheritance.java
+++ SimpleInheritance.java
@@ -8,43 +8,52 @@
  *
  * @author DMyrzaka
  */
- class A{
-    int i,j;
+class A{
+int i,j;
+
+
     void showij(){
-        System.out.println("i and j: "+i+" "+j);
+        System.out.println("i and j "+i+" "+j);
     }
 }
-
 class B extends A{
     int k;
     void showk(){
-        System.out.println("k: "+k);  
+        System.out.println("k: "+k);
     }
-    
     void sum(){
-        System.out.println("i+j+k: "+(i+j+k));
+        System.out.println("i+j+k:"+(i+j+k));
     }
 }
-public class SimpleInheritance {
-    public static void main(String args[]){
-        A superOb = new A();
-        B subOb = new B();
-        
+class SimpleInheritance {
+    public static void main (String args[]){
+       A superOb=new A();//объект и ссылка на класс
+       B subOb=new B();
+        //SimpleInheritance sss=new SimpleInheritance sss=();
+        //System.out.println(" "+new A().j);
+        //superOb.showij();
         superOb.i=10;
-        superOb.j=20;
-        System.out.println("Content superOb: ");
+        superOb.j=10;
+        System.out.println("Содержимое suoerOb: ");
         superOb.showij();
         System.out.println();
         
         subOb.i=7;
         subOb.j=8;
         subOb.k=9;
-        System.out.println("Content subOb: ");
+        
+        superOb.showij();//не удалять!
+        
+        subOb.showij();
+        System.out.println("Содержимое subOb: ");
         subOb.showij();
         subOb.showk();
         
         System.out.println();
-        System.out.println("Sum i,j and k in subOb:");
+        System.out.println("Сумма i, j и k в subOb:");
         subOb.sum();
+                
     }
 }
+        
+
