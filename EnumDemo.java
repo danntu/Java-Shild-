912fcf3b093f1ef5a3f2dcbd46977fde8b414eb4
Daniyar <dan_ntu@mail.ru>
2017-05-05 11:41:26 +0600

    Add files via upload

---
+++ EnumDemo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package glava12;
+
+/**
+ *
+ * @author DMyrzaka
+ */
+enum Apple{
+    Jonathan, GoldenDel, RedDel, Winesap,Cortland
+}
+public class EnumDemo {
+    public static void main(String[] args) {
+        Apple ap; 
+        ap = Apple.RedDel;
+        System.out.println("Значение ap: "+ap);
+        System.out.println();
+        
+        ap =Apple.GoldenDel;
+        
+        if (ap==Apple.GoldenDel) System.out.println("ap содержит ColdenDel.\n");
+        
+        switch(ap){
+            case Jonathan: System.out.println("Jhonatan"); break;
+            case GoldenDel: System.out.println("Golden Delicious");
+        }
+    }
+}
