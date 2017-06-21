82abb601c9d59f52de7571d9ffd319258796386a
Daniyar <dan_ntu@mail.ru>
2017-06-21 17:23:36 +0600

    Add files via upload

--- FormatDemo.java
+++ FormatDemo.java
@@ -11,9 +11,11 @@ package glava18;
 import java.util.Formatter;
 public class FormatDemo {
     public static void main(String args[]){
-        Formatter fmt = new Formatter();
-        fmt.format("Formatting %s simple %d %f","is",10,98.6);
+        try (Formatter fmt = new Formatter()){
+            fmt.format("Formatting %s simple %d %f","is",10,98.6);
         System.out.println(fmt);
-        fmt.close();
+        }
+        
+       
     }
 }
