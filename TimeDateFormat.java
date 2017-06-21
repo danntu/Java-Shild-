6f0ab2102ca5e1e9d26e0ddfd3e82f3d1eba7c9c
Daniyar <dan_ntu@mail.ru>
2017-06-21 15:55:31 +0600

    format time

---
+++ TimeDateFormat.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package glava18;
+
+import java.util.Calendar;
+import java.util.Formatter;
+
+/**
+ *
+ * @author DMyrzaka
+ */
+public class TimeDateFormat {
+    public static void main(String args[]){
+        Formatter fmt = new Formatter();
+        Calendar cal = Calendar.getInstance();
+        
+        fmt.format("%tr", cal);
+        System.out.println(fmt);
+        
+        fmt= new Formatter();
+        fmt.format("%tc",cal);
+        System.out.println(fmt);
+        
+        fmt= new Formatter();
+        fmt.format("%tl:%tM",cal,cal);
+        System.out.println(fmt);
+        
+        fmt= new Formatter();
+        fmt.format("%tB %tb %tm",cal,cal,cal);
+        System.out.println(fmt);
+        fmt.close();
+        
+    }
+}
