d7a3ed65c82a801e9d9c2fa3086e5fb3978c53f0
Daniyar <dan_ntu@mail.ru>
2017-06-27 10:09:04 +0600

    class PushbackInputStream

---
+++ PushbackInputStreamDemo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package glava19;
+
+/**
+ *
+ * @author DMyrzaka
+ */
+import java.io.*;
+public class PushbackInputStreamDemo {
+    public static void main(String args[]){
+        String s = "if (a == 4) a=0;\n";
+        
+        byte buf[]=s.getBytes();
+        ByteArrayInputStream in = new ByteArrayInputStream(buf);
+        int c; 
+        try(PushbackInputStream f = new PushbackInputStream(in)){
+            while((c=f.read()) !=-1){
+                switch(c){
+                    case '=':
+                        if ((c=f.read())=='=')
+                            System.out.print(".eq.");
+                        else {
+                            System.out.print("<-");
+                            f.unread(c);
+                        }
+                        break;
+                        default:
+                            System.out.print((char)c);
+                            break;
+                }
+            }
+        } catch(IOException e){
+            System.out.println("I/O Error: "+e);
+        }
+    }
+}
