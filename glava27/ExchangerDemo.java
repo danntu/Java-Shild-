373bb99e58021db5e2c2e33b7006a83fe1525d9e
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-07-31 10:37:00 +0600

    class Exchager

--- glava27/ExchangerDemo.java
+++ glava27/ExchangerDemo.java
@@ -1,7 +1,18 @@
 package glava27;
 
+import java.util.concurrent.Exchanger;
+
 public class ExchangerDemo {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Exchanger<String> exgr = new Exchanger<String>();
+    }
+}
+class MakeString implements Runnable{
+    Exchanger<String> ex;
+    String str;
+
+    public MakeString(Exchanger<String> ex) {
+        this.ex = ex;
+        str= new
     }
 }
