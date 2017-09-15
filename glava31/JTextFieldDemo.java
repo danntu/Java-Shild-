e713e8db733b460700ecfced67774974bc17192e
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-09-15 11:43:51 +0600

    class JTextField used different methods

---
+++ glava31/JTextFieldDemo.java
@@ -0,0 +1,36 @@
+package glava31;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class JTextFieldDemo extends JApplet{
+    @Override
+    public void init() {
+        try{
+            SwingUtilities.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    makeGUI();
+                }
+            });
+        } catch(Exception e){
+            System.out.println("Can't create because of "+e);
+        }
+    }
+    private void makeGUI(){
+        JTextField textField = new JTextField("Please enter text",15);
+        textField.setForeground(Color.blue);
+        textField.setBorder(BorderFactory.createLineBorder(Color.RED,3));
+        textField.setBackground(Color.ORANGE);
+        textField.setFont(new Font("Arial",20,20));
+        add(textField);
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showStatus(textField.getText());
+            }
+        });
+    }
+}
