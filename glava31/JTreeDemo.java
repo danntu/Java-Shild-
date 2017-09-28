e8a63ee772fd67168f10a972d0690b74c71ececb
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-09-28 14:00:51 +0600

    class JTree

---
+++ glava31/JTreeDemo.java
@@ -0,0 +1,56 @@
+package glava31;
+
+import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.awt.*;
+
+public class JTreeDemo extends JApplet {
+    JTree jTree;
+    JLabel jLabel;
+
+    @Override
+    public void init() {
+        try{
+            SwingUtilities.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    makeGUI();
+                }
+            });
+        }catch(Exception e){
+            System.out.println("Can't create because of "+e);
+        }
+    }
+    private void makeGUI(){
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode("Oprions");
+
+        DefaultMutableTreeNode a  = new DefaultMutableTreeNode("A");
+        top.add(a);
+        DefaultMutableTreeNode a1 = new DefaultMutableTreeNode("A1");
+        a.add(a1);
+        DefaultMutableTreeNode a2 = new DefaultMutableTreeNode("A2");
+        a.add(a2);
+
+        DefaultMutableTreeNode b = new DefaultMutableTreeNode("B");
+        top.add(b);
+        DefaultMutableTreeNode b1 = new DefaultMutableTreeNode("B1");
+        b.add(b1);
+        DefaultMutableTreeNode b2 = new DefaultMutableTreeNode("B2");
+        b.add(b2);
+        DefaultMutableTreeNode b3 = new DefaultMutableTreeNode("B3");
+        b.add(b3);
+        jTree = new JTree(top);
+        JScrollPane jsp = new JScrollPane(jTree);
+        add(jsp);
+        jLabel = new JLabel();
+        add(jLabel, BorderLayout.SOUTH);
+        jTree.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                jLabel.setText("Selection is "+e.getPath());
+            }
+        });
+    }
+}
