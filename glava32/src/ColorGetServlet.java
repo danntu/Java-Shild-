d75f15ec93d00dcdbfa31215f7bcdb64c3c75122
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-09-29 15:16:27 +0600

    servlet Example

---
+++ glava32/src/ColorGetServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ColorGetServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException,IOException{
+        String color = req.getParameter("color");
+        res.setContentType("text/html");
+        PrintWriter pw = res.getWriter();
+        pw.println("<b>The selected color is: ");
+        pw.println(color);
+        pw.close();
+    }
+}
