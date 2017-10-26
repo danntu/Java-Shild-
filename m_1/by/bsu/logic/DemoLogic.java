0ce991341788deb83dccad26bb437342d7fbe2ff
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-10-26 14:34:55 +0600

    Java Методы программирования стр 67

---
+++ m_1/by/bsu/logic/DemoLogic.java
@@ -0,0 +1,9 @@
+package m_1.by.bsu.logic;
+
+public class DemoLogic {
+    public static void main(String[] args) {
+        new Department(71);
+        new Department(17);
+
+    }
+}
---
+++ m_1/by/bsu/logic/Department.java
@@ -0,0 +1,23 @@
+package m_1.by.bsu.logic;
+
+public class Department {
+    {
+        System.out.println("logic(1) id ="+this.id);
+    }
+    static {
+        System.out.println("static logic");
+    }
+    private int id =7;
+
+    public Department(int id) {
+        this.id=id;
+        System.out.println("конструктор id="+id);
+    }
+
+    public int getId() {
+        return id;
+    }
+    {
+        System.out.println("logic (2) id="+id);
+    }
+}
