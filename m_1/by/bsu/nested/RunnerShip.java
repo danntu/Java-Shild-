f127fc44ce7ca26157c57ea49cccea4345ef771e
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-11-03 15:46:33 +0600

    Java Методы программирования стр 140. Nested class

---
+++ m_1/by/bsu/nested/RunnerShip.java
@@ -0,0 +1,9 @@
+package m_1.by.bsu.nested;
+
+public class RunnerShip {
+    public static void main(String[] args) {
+        Ship.LifeBoat.down();
+        Ship.LifeBoat lifeBoat = new Ship.LifeBoat();
+        lifeBoat.swim();
+    }
+}
---
+++ m_1/by/bsu/nested/Ship.java
@@ -0,0 +1,15 @@
+package m_1.by.bsu.nested;
+
+public class Ship {
+    private int id;
+
+    public static class LifeBoat{
+        private int boatId;
+        public static void down(){
+            System.out.println("шлюпки на воду!");
+        }
+        public void swim(){
+            System.out.println("отплытие шлюпки");
+        }
+    }
+}
