3ccb4d56acd1d5cc37697044d4fe6dca04d1c439
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-11-03 15:50:06 +0600

    Java Методы программирования стр 141. Nested class

---
+++ m_1/by/bsu/nested/University.java
@@ -0,0 +1,15 @@
+package m_1.by.bsu.nested;
+
+public interface University {
+    int NUMBER_FACULTY = 20;
+    void create();
+    class LearningDepartment{
+        public int idChief;
+        public static void assignPlan(int idFaculty){
+
+        }
+        public void acceptProgram(){
+
+        }
+    }
+}
