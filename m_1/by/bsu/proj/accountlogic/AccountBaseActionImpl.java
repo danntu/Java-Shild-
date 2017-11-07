da2acf73f973476ab5d926b949f0685c4f3a849e
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-11-07 12:01:23 +0600

    Java Методы программирования стр 153. Interface implementation

---
+++ m_1/by/bsu/proj/accountlogic/AccountBaseActionImpl.java
@@ -0,0 +1,23 @@
+package m_1.by.bsu.proj.accountlogic;
+
+public class AccountBaseActionImpl implements AccountBaseAction {
+    @Override
+    public boolean openAccount() {
+        return false;
+    }
+
+    @Override
+    public boolean closeAccount() {
+        return false;
+    }
+
+    @Override
+    public void blocking() {
+
+    }
+
+    @Override
+    public void unBlocking() {
+
+    }
+}
