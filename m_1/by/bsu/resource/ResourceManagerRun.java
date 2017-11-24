7c391490adfe7ee6651136ecc030268e62a499e6
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-11-24 12:56:03 +0600

    Java Методы программирования стр 222. assert

--- m_1/by/bsu/resource/ResourceManagerRun.java
+++ m_1/by/bsu/resource/ResourceManagerRun.java
@@ -4,9 +4,12 @@ import java.util.Locale;
 
 public class ResourceManagerRun {
     public static void main(String[] args) {
-        ResourceManager manager = ResourceManager.INSTANCE;
-        System.out.println(manager.getString("str1"));
-        manager.changeResource(new Locale("be","BY"));
-        System.out.println(manager.getString("str1"));
+        int a =-1;
+        assert (a>0) : "NEGATIVE";
+        //jvm java -ea ResourceManagerRun
+//        ResourceManager manager = ResourceManager.INSTANCE;
+//        System.out.println(manager.getString("str1"));
+//        manager.changeResource(new Locale("be","BY"));
+//        System.out.println(manager.getString("str1"));
     }
 }
