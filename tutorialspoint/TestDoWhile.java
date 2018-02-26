d80e7a68f468314765fc3b29e9b7b9c6356d8b34
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-26 11:09:32 +0600

    Tutorialspoint. Java do while loop

---
+++ tutorialspoint/TestDoWhile.java
@@ -0,0 +1,10 @@
+public class TestDoWhile {
+    public static void main(String[] args) {
+        int x = 10;
+        do {
+            System.out.print("value of x :" + x);
+            x++;
+            System.out.println();
+        } while (x < 20);
+    }
+}
