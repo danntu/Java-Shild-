acc7b54afed566ab453f2176ca8e66091cc65baa
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-26 15:45:24 +0600

    Tutorialspoint. Java. Class Number xxxValue() method

---
+++ tutorialspoint/NumberxxxValue.java
@@ -0,0 +1,8 @@
+public class NumberxxxValue {
+    public static void main(String[] args) {
+        Integer x = 5;
+        System.out.println(x.byteValue());
+        System.out.println(x.doubleValue());
+        System.out.println(x.longValue());
+    }
+}
