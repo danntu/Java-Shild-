3a16d023b402597bec3a9f4f7ea662170dbf8fac
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-26 13:45:36 +0600

    Tutorialspoint. Java if Example

--- tutorialspoint/IfExample.java
+++ tutorialspoint/IfExample.java
@@ -2,7 +2,7 @@ public class IfExample {
     public static void main(String[] args) {
         int x =5;
         if (x<20){
-            System.out.println("This is if statment");
+            System.out.println("This is if statement");
         }
     }
 }
