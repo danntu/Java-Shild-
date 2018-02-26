54bc182ddf8b316723d7b449ffa69c79e97ab019
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-26 16:39:05 +0600

    Tutorialspoint. Java. Class Number floor,ceil, round() etc method

---
+++ tutorialspoint/Numberceilfloor.java
@@ -0,0 +1,24 @@
+public class Numberceilfloor {
+    public static void main(String[] args) {
+        double d = 100.78;
+        double f =91.45;
+        System.out.println("ceil");
+        System.out.println(Math.ceil(d));
+        System.out.println(Math.ceil(f));
+        System.out.println("floor");
+        System.out.println(Math.floor(d));
+        System.out.println(Math.floor(f));
+        System.out.println("round");
+        System.out.println(Math.round(d));
+        System.out.println(Math.round(f));
+        System.out.println("rint");
+        System.out.println(Math.rint(d));
+        System.out.println(Math.rint(f));
+        System.out.println("\"min()\" \f\'dfdf\'");
+        System.out.println(Math.min(d,f));
+        System.out.println("max()\\");
+        System.out.println(Math.max(d,f));
+        System.out.println("random");
+        System.out.println(Math.round(Math.random()*10));
+    }
+}
