79a4a51d3d086d101238a26fdc639e8c6cc305e6
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-26 15:49:23 +0600

    Tutorialspoint. Java. Class Number compareTo() method

---
+++ tutorialspoint/NumbercompareTo.java
@@ -0,0 +1,8 @@
+public class NumbercompareTo {
+    public static void main(String[] args) {
+        Integer x = 5;
+        System.out.println(x.compareTo(3));
+        System.out.println(x.compareTo(5));
+        System.out.println(x.compareTo(88));
+    }
+}
