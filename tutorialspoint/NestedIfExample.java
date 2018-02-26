bce8e38ad6de56ee9da296d40290f1c69f85a185
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-26 13:53:05 +0600

    Tutorialspoint. Java nested if Example

---
+++ tutorialspoint/NestedIfExample.java
@@ -0,0 +1,11 @@
+public class NestedIfExample {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 10;
+        if (x==30){
+            if(y==10){
+                System.out.println("X = 30 and Y = 10");
+            }
+        }
+    }
+}
