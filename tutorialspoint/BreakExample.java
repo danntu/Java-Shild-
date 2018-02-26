d23ef5e287b6dea9a8585974fe8baa1ceac578fc
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-26 11:11:58 +0600

    Tutorialspoint. Java break

---
+++ tutorialspoint/BreakExample.java
@@ -0,0 +1,10 @@
+public class BreakExample {
+    public static void main(String[] args) {
+        int num[] = {1, 2, 3, 4, 5};
+        for (int x :
+                num) {
+            System.out.print("value of x :" + x);
+            System.out.println();
+        }
+    }
+}
