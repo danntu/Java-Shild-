a23ac0e61c44a031e85423b23e5485f234ef0b43
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-27 17:19:09 +0600

    Tutorialspoint. Java. Byte Streans

--- tutorialspoint/io/CopyFileInputOutputStream.java
+++ tutorialspoint/io/CopyFileInputOutputStream.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class CopyFile {
+public class CopyFileInputOutputStream {
     public static void main(String[] args) throws IOException {
         FileInputStream fin =null;
         FileOutputStream fout = null;
