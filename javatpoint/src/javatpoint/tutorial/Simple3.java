18ed4fbfcb66ab5a2d8ee9a6557de7f4418b50a7
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-03-28 11:57:05 +0600

    Java Variable Example: Widening

---
+++ javatpoint/src/javatpoint/tutorial/Simple3.java
@@ -0,0 +1,10 @@
+package javatpoint.tutorial;
+
+public class Simple3 {
+    public static void main(String[] args) {
+        int a = 10;
+        float f = a;
+        System.out.println(a);
+        System.out.println(f);
+    }
+}
