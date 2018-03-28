cd1822910080a0c16a1690929a1510b26c35469c
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-03-28 11:55:55 +0600

    Java Variable Example: Add Two Numbers

---
+++ javatpoint/src/javatpoint/tutorial/Simple2.java
@@ -0,0 +1,10 @@
+package javatpoint.tutorial;
+
+public class Simple2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
