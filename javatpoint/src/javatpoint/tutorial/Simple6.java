9954aed2b1f37d03fc414a911f011a43e5ec6901
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-03-28 12:12:45 +0600

    Java Variable Example: Adding Lower Type

---
+++ javatpoint/src/javatpoint/tutorial/Simple6.java
@@ -0,0 +1,11 @@
+package javatpoint.tutorial;
+
+public class Simple6 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 10;
+
+        byte c = (byte) (a + b);
+        System.out.println(c);
+    }
+}
