bda2c72a7d481d35ed5e1cb75f3921e3075ab82c
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-03-28 14:01:03 +0600

    Java Ternary Operator Example

---
+++ javatpoint/src/javatpoint/tutorial/OpertorExample9.java
@@ -0,0 +1,14 @@
+package javatpoint.tutorial;
+
+public class OpertorExample9 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int min = (a < b) ? a : b;
+        System.out.println(min);
+        a = 10;
+        b = 5;
+        min = (a < b) ? a : b;
+        System.out.println(min);
+    }
+}
