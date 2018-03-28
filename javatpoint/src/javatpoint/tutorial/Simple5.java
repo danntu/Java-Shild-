0495eac7cff882da2a8c4cef9d371f66ef85b3cb
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-03-28 12:04:43 +0600

    Java Variable Example: Overflow

---
+++ javatpoint/src/javatpoint/tutorial/Simple5.java
@@ -0,0 +1,10 @@
+package javatpoint.tutorial;
+
+public class Simple5 {
+    public static void main(String[] args) {
+        int a = 129;
+        byte b = (byte) a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
