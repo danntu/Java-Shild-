90e4d8f21abb0791eb02b986d9167cff48f0fc8f
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-09 14:51:14 +0600

    Java Multithreading. Daemon Thread in Java

---
+++ javatpoint/src/javatpoint/multithreading/TestDaemonThread1.java
@@ -0,0 +1,26 @@
+package javatpoint.multithreading;
+
+import org.junit.Test;
+
+public class TestDaemonThread1 extends Thread {
+    @Override
+    public void run() {
+        if (Thread.currentThread().isDaemon()) {
+            System.out.println("daemon thread work");
+        } else {
+            System.out.println("user thread work");
+        }
+    }
+
+    public static void main(String[] args) {
+        TestDaemonThread1 t1 = new TestDaemonThread1();
+        TestDaemonThread1 t2 = new TestDaemonThread1();
+        TestDaemonThread1 t3 = new TestDaemonThread1();
+
+        t1.setDaemon(true);
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+}
