7557836e42f7c97b8d179784d4f613f22b184c74
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-11 11:38:57 +0600

    Java FileOutputStream example 2: write string

--- javatpoint/src/javatpoint/io/FileOutputStreamExample.java
+++ javatpoint/src/javatpoint/io/FileOutputStreamExample.java
@@ -8,7 +8,10 @@ public class FileOutputStreamExample {
     public static void main(String[] args) throws IOException {
 
         FileOutputStream fout = new FileOutputStream("testout.txt");
+        String s = "Welcome to javaTpoint.";
+        byte[] b = s.getBytes();
         fout.write(65);
+        fout.write(b);
         fout.close();
         System.out.println("success");
 
