8cadf709dcac29d652afa9f167dd8c4bfe7c9313
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-13 10:15:10 +0600

    Java Console Example to read password. Works only console without IDE

---
+++ javatpoint/src/javatpoint/io/ReadPasswordTest.java
@@ -0,0 +1,13 @@
+package javatpoint.io;
+
+import java.io.Console;
+
+public class ReadPasswordTest {
+    public static void main(String[] args) {
+        Console c = System.console();
+        System.out.println("Enter password");
+        char[] ch = c.readPassword();
+        String pass = String.valueOf(ch);
+        System.out.println("Password is: " + pass);
+    }
+}
