c3e1b8c13bc044087c90fab067477930869eac1b
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-13 10:11:37 +0600

    Java Console Example. Works only console without IDE

---
+++ javatpoint/src/javatpoint/io/ReadStringTest.java
@@ -0,0 +1,12 @@
+package javatpoint.io;
+
+import java.io.Console;
+
+public class ReadStringTest {
+    public static void main(String[] args) {
+        Console c = System.console();
+        System.out.println("Enter your name: ");
+        String n = c.readLine();
+        System.out.println("Welcome " + n);
+    }
+}
