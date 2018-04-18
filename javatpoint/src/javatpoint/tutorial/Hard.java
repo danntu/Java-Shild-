60a4435dc2073a0fc38e0275b4fb9aecf65de028
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-18 09:39:41 +0600

    Filename Hard.java by classLoader is SimpleTest

---
+++ javatpoint/src/javatpoint/tutorial/Hard.java
@@ -0,0 +1,19 @@
+package javatpoint.tutorial;
+
+class Simpletest {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
+class A {
+
+}
+
+class B {
+
+}
+
+class C {
+
+}
