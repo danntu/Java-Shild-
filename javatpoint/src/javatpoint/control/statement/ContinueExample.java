afb547a16043f619e623f2bdb4c930f823b043f9
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-18 11:02:40 +0600

    Javatpoint. Java Continue Statement

---
+++ javatpoint/src/javatpoint/control/statement/ContinueExample.java
@@ -0,0 +1,25 @@
+package javatpoint.control.statement;
+
+public class ContinueExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
+
+class ContinueExample2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (i == 2 && j == 2) {
+                    continue;
+                }
+                System.out.println(i + " " + j);
+            }
+        }
+    }
+}
