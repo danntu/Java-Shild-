2f72b5c827bf11e9329c1bd09da8bf715b206bef
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-18 11:01:20 +0600

    Javatpoint. Java Break Statement

---
+++ javatpoint/src/javatpoint/control/statement/BreakExample.java
@@ -0,0 +1,25 @@
+package javatpoint.control.statement;
+
+public class BreakExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
+
+class BreakExample2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (i == 2 && j == 2) {
+                    break;
+                }
+                System.out.println(i + " " + j);
+            }
+        }
+    }
+}
