d4a2d0b195e595d0d2c7b83c23d5176061b95ff5
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-18 10:57:46 +0600

    Javatpoint. Loop while

---
+++ javatpoint/src/javatpoint/control/statement/WhileExample.java
@@ -0,0 +1,19 @@
+package javatpoint.control.statement;
+
+public class WhileExample {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+class WhileExample2 {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("infinitive while loop");
+        }
+    }
+}
