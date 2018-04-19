9073f25a2574686a1f06a07467591ec2ea773c4c
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-19 16:20:19 +0600

    Javatpoint. Object and Class Example: main within class

---
+++ javatpoint/src/javatpoint/object_class/Student.java
@@ -0,0 +1,12 @@
+package javatpoint.object_class;
+
+public class Student {
+    int id;
+    String name;
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
