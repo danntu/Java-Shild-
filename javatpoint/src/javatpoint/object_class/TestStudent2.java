50d749339ec34e3cebd876e02b1bf6a120258199
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-19 16:26:00 +0600

    Javatpoint. Object and Class Example: Initialization through reference

---
+++ javatpoint/src/javatpoint/object_class/TestStudent2.java
@@ -0,0 +1,10 @@
+package javatpoint.object_class;
+
+public class TestStudent2 {
+    public static void main(String[] args) {
+        Student1 s1 = new Student1();
+        s1.id = 101;
+        s1.name = "Danik";
+        System.out.println(s1.id + " " + s1.name);
+    }
+}
