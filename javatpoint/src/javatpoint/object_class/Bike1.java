4a0c61d7d497c089892c587f8d2323785e950b2f
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-20 10:29:56 +0600

    Javatpoint. Example of default constructor

---
+++ javatpoint/src/javatpoint/object_class/Bike1.java
@@ -0,0 +1,11 @@
+package javatpoint.object_class;
+
+public class Bike1 {
+    Bike1() {
+        System.out.println("Bike is created");
+    }
+
+    public static void main(String[] args) {
+        Bike1 b = new Bike1();
+    }
+}
