b2142d42af61bc134823b0e8017577fa02a6718e
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-25 14:55:47 +0600

    Javatpoint. Java Inheritance Example

---
+++ javatpoint/src/javatpoint/inheritance/Programmer.java
@@ -0,0 +1,15 @@
+package javatpoint.inheritance;
+
+class Employee {
+    float salary = 40000;
+}
+
+public class Programmer extends Employee {
+    int bonus = 10000;
+
+    public static void main(String[] args) {
+        Programmer p = new Programmer();
+        System.out.println("Programmer salary is: " + p.salary);
+        System.out.println("Bonus pf Programmer is: " + p.bonus);
+    }
+}
