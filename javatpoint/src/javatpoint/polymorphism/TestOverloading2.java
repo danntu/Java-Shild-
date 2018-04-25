c1bff0e6f07b5da934dbbe13c0db4ae68354649c
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-25 15:54:33 +0600

    Javatpoint. Method Overloading: changing data type of arguments

---
+++ javatpoint/src/javatpoint/polymorphism/TestOverloading2.java
@@ -0,0 +1,18 @@
+package javatpoint.polymorphism;
+
+class Adder2 {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class TestOverloading2 {
+    public static void main(String[] args) {
+        System.out.println(Adder2.add(11, 11));
+        System.out.println(Adder2.add(12.3, 12.6));
+    }
+}
