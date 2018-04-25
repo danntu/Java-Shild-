dd1e7a8c52d5b76ad21b278aaf6023ef90dc03b2
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-25 11:22:05 +0600

    Javatpoint. Example of static block

---
+++ javatpoint/src/javatpoint/object_class/A2.java
@@ -0,0 +1,11 @@
+package javatpoint.object_class;
+
+public class A2 {
+    static {
+        System.out.println("static block is invoked");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello main");
+    }
+}
