b1a3380c5e6c87fb491e5d1924c3c17e5e6481cb
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-26 09:07:33 +0600

    Javatpoint. Simple example of Covariant Return Type

---
+++ javatpoint/src/javatpoint/polymorphism/TestSuper1.java
@@ -0,0 +1,21 @@
+package javatpoint.polymorphism;
+
+class Animal {
+    String color = "white";
+}
+
+class Dog extends Animal {
+    String color = "black";
+
+    void printColor() {
+        System.out.println(color); // print color of Dog class
+        System.out.println(super.color); // print color of Animal class
+    }
+}
+
+public class TestSuper1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.printColor();
+    }
+}
