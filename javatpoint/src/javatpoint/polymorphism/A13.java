98f8e87a2fb54349e78f70b23bccbc247892c235
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-26 14:10:04 +0600

    Javatpoint. Example of static blank final variable

---
+++ javatpoint/src/javatpoint/polymorphism/A13.java
@@ -0,0 +1,13 @@
+package javatpoint.polymorphism;
+
+public class A13 {
+    static final int data;
+
+    static {
+        data = 50;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(A13.data);
+    }
+}
