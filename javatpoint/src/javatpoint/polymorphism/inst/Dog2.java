ad3d07bb5929572b02332938f3bffcb60abbb664
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-26 15:38:26 +0600

    Javatpoint. instanceof in java with a variable that have null value

---
+++ javatpoint/src/javatpoint/polymorphism/inst/Dog2.java
@@ -0,0 +1,8 @@
+package javatpoint.polymorphism.inst;
+
+public class Dog2 {
+    public static void main(String[] args) {
+        Dog2 dog2 = null;
+        System.out.println(dog2 instanceof Dog2);
+    }
+}
