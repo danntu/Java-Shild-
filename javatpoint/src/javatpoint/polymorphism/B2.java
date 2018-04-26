72a8f574f0057ad1a520367c0d30544e0cedabd1
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-26 09:31:46 +0600

    Javatpoint. Program of instance initializer block that is invoked after super()

---
+++ javatpoint/src/javatpoint/polymorphism/B2.java
@@ -0,0 +1,22 @@
+package javatpoint.polymorphism;
+
+class A2 {
+    public A2() {
+        System.out.println("parent class constructor invoked");
+    }
+}
+
+public class B2 extends A2 {
+    public B2() {
+        super();
+        System.out.println("child class constructor invoked");
+    }
+
+    {
+        System.out.println("instance initializer block is invoked");
+    }
+
+    public static void main(String[] args) {
+        B2 b2 = new B2();
+    }
+}
