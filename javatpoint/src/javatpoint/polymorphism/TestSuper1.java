029ee1da478e41d1a0c59bc16503977cb5cd5f00
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-26 09:08:07 +0600

    Javatpoint. super is used to refer immediate parent class instance variable.

--- javatpoint/src/javatpoint/polymorphism/TestSuper1.java
+++ javatpoint/src/javatpoint/polymorphism/TestSuper1.java
@@ -6,7 +6,6 @@ class Animal {
 
 class Dog extends Animal {
     String color = "black";
-
     void printColor() {
         System.out.println(color); // print color of Dog class
         System.out.println(super.color); // print color of Animal class
