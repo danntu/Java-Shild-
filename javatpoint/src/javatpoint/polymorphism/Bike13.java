9a32d1e8d87974257848ab793edb276120890f78
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-26 14:07:52 +0600

    Javatpoint. Can we initialize blank final variable? Yes, but only in constructor.

---
+++ javatpoint/src/javatpoint/polymorphism/Bike13.java
@@ -0,0 +1,14 @@
+package javatpoint.polymorphism;
+
+public class Bike13 {
+    final int speedlimit;
+
+    public Bike13() {
+        speedlimit = 70;
+        System.out.println(speedlimit);
+    }
+
+    public static void main(String[] args) {
+        new Bike13();
+    }
+}
