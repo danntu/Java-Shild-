f2caa097de4131a1569937a81f48fe3b9188d498
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-04-26 14:00:36 +0600

    Javatpoint. Java final class

---
+++ javatpoint/src/javatpoint/polymorphism/Honda11.java
@@ -0,0 +1,16 @@
+package javatpoint.polymorphism;
+
+//class Bike11 cannot inheritance
+final class Bike11 {
+}
+
+public class Honda11 extends Bike11 {
+    void run() {
+        System.out.println("running safely with 100kmph");
+    }
+
+    public static void main(String[] args) {
+        Honda11 honda11 = new Honda11();
+        honda11.run();
+    }
+}
