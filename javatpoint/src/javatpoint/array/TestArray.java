d394e66ffd2b49f3664be5237de1dcebf19696e3
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-03 11:04:48 +0600

    Javatpoint. Example of single dimensional java array

---
+++ javatpoint/src/javatpoint/array/TestArray.java
@@ -0,0 +1,16 @@
+package javatpoint.array;
+
+public class TestArray {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        a[0] = 10;//initialization
+        a[1] = 20;
+        a[2] = 70;
+        a[3] = 40;
+        a[4] = 50;
+        //print array
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "] = " + a[i]);
+        }
+    }
+}
