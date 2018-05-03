dfefb1bb5476b6d5d3636b5adbb53d5ed95c7cd7
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-03 11:00:05 +0600

    Javatpoint. Simple example of encapsulation in java

---
+++ javatpoint/src/javatpoint/encapsulation/Test.java
@@ -0,0 +1,9 @@
+package javatpoint.encapsulation;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Danik");
+        System.out.println("Student name is " + student.getName());
+    }
+}
