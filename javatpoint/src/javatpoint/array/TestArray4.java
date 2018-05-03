3803dc025976dbc6ceddeda5326d02de1ad1a255
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-03 11:23:18 +0600

    Javatpoint. What is the class name of java array?

---
+++ javatpoint/src/javatpoint/array/TestArray4.java
@@ -0,0 +1,12 @@
+package javatpoint.array;
+
+import java.util.StringJoiner;
+
+public class TestArray4 {
+    public static void main(String[] args) {
+        int a[] = {4, 4, 5, 7};
+        Class c = a.getClass();
+        String name = c.getName();
+        System.out.println(name);
+    }
+}
