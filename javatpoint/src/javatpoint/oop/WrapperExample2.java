4b1dfcf23e8aaa42602a022ea70af60c81484c25
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-04 14:38:21 +0600

    Javatpoint. Wrapper class Example: Wrapper to Primitive

---
+++ javatpoint/src/javatpoint/oop/WrapperExample2.java
@@ -0,0 +1,9 @@
+package javatpoint.oop;
+
+public class WrapperExample2 {
+    public static void main(String[] args) {
+        Integer a = new Integer(3);
+        int i = a.intValue(); //convert Integer to int
+        int j = a; //unboxing
+    }
+}
