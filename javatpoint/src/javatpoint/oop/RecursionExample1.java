973478d6b2f3237ab9531ace124d093b3843f474
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-04 14:59:29 +0600

    Javatpoint. Java Recursion Example 1: Infinite times

---
+++ javatpoint/src/javatpoint/oop/RecursionExample1.java
@@ -0,0 +1,14 @@
+package javatpoint.oop;
+
+public class RecursionExample1 {
+    static void p() {
+        System.out.println("hello");
+        p();
+    }
+
+    public static void main(String[] args) {
+        p();
+
+
+    }
+}
