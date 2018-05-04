581ae256dbcc2f541c35963a6ce33b90748e4197
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-04 15:12:30 +0600

    Javatpoint. Java Recursion Example 3: Factorial Number

---
+++ javatpoint/src/javatpoint/oop/RecursionExample3.java
@@ -0,0 +1,13 @@
+package javatpoint.oop;
+
+public class RecursionExample3 {
+    static int fact(int n) {
+        if (n == 1) return 1;
+        else
+            return (n * fact(n - 1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial of 5 is: " + fact(3));
+    }
+}
