0226edcfa2134144935a78fa72dac4bfb403b476
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-10 14:39:02 +0600

    Javatpoint. Example of java substring

---
+++ javatpoint/src/javatpoint/str/TestSubstring.java
@@ -0,0 +1,9 @@
+package javatpoint.str;
+
+public class TestSubstring {
+    public static void main(String[] args) {
+        String s = "DaniyarMyrzakanov";
+        System.out.println(s.substring(7));
+        System.out.println(s.substring(0, 7));
+    }
+}
