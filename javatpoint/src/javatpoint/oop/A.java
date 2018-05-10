ba30c8f763efdca8e7fc0cdc7d52723285097e33
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-10 10:02:24 +0600

    Javatpoint. Example of command-line argument that prints all the values

---
+++ javatpoint/src/javatpoint/oop/A.java
@@ -0,0 +1,9 @@
+package javatpoint.oop;
+
+public class A {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
