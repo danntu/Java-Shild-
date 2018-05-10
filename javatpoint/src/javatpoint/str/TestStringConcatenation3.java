c27b3697d451f9c77e2cc4552ccd4c51b0f60d79
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-10 14:33:37 +0600

    Javatpoint. String Concatenation by concat() method

---
+++ javatpoint/src/javatpoint/str/TestStringConcatenation3.java
@@ -0,0 +1,10 @@
+package javatpoint.str;
+
+public class TestStringConcatenation3 {
+    public static void main(String[] args) {
+        String s1 = "Myrzakanov ";
+        String s2 = "Daniyar";
+        String s3 = s1.concat(s2);
+        System.out.println(s3);
+    }
+}
