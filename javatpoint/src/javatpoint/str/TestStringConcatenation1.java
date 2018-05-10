ffbc4e1f1ddecd7be1fd8fff8537771ebfece7ba
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-10 14:31:51 +0600

    Javatpoint. String Concatenation by + (string concatenation) operator

---
+++ javatpoint/src/javatpoint/str/TestStringConcatenation1.java
@@ -0,0 +1,15 @@
+package javatpoint.str;
+
+public class TestStringConcatenation1 {
+    public static void main(String[] args) {
+        String s = "Myrzakanov " + "Daniyar";
+        System.out.println(s);
+    }
+}
+
+class TestStringConcatenation2 {
+    public static void main(String[] args) {
+        String s = 50 + 30 + " Danik" + 40 + 40;
+        System.out.println(s);
+    }
+}
