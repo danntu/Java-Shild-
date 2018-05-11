370dfca5755e68009c67b1b764855edfca9f20bc
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-11 15:22:22 +0600

    Javatpoint. Understanding problem without toString() method

---
+++ javatpoint/src/javatpoint/str/Student.java
@@ -0,0 +1,20 @@
+package javatpoint.str;
+
+public class Student {
+    int rollno;
+    String name;
+    String city;
+
+    public Student(int rollno, String name, String city) {
+        this.rollno = rollno;
+        this.name = name;
+        this.city = city;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student(101, "Danik", "Almaty");
+        Student student1 = new Student(102, "Moldir", "Astana");
+        System.out.println(student);
+        System.out.println(student1);
+    }
+}
