50c1288682ceb6dfe8af9bdfabc4d4636824310b
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-11 10:06:47 +0600

    Javatpoint. StringBuilder append() method

---
+++ javatpoint/src/javatpoint/str/StringBuilderExample.java
@@ -0,0 +1,9 @@
+package javatpoint.str;
+
+public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Hello");
+        stringBuilder.append(" Java");
+        System.out.println(stringBuilder);
+    }
+}
