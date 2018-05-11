7054bf803b3b082886f26d7aad9c0595796f20f7
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-11 10:45:21 +0600

    Javatpoint. StringBuffer Example

---
+++ javatpoint/src/javatpoint/str/BufferTest.java
@@ -0,0 +1,9 @@
+package javatpoint.str;
+
+public class BufferTest {
+    public static void main(String[] args) {
+        StringBuffer stringBuffer = new StringBuffer("hello");
+        stringBuffer.append("java");
+        System.out.println(stringBuffer);
+    }
+}
