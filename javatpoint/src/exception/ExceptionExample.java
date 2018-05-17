793c1c3dae957795df506f38a7e1ba01d10041f0
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-17 09:00:52 +0600

    Javatpoint. Exception example for Runtime exception

---
+++ javatpoint/src/exception/ExceptionExample.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class ExceptionExample {
+    public static void main(String[] args) {
+        //int a =50/0;
+        //String s = null;
+        //System.out.println(s.length());
+        //String s = "abc";
+        //int i = Integer.parseInt(s);
+        int a[] = new int[5];
+        a[10] = 50;
+    }
+}
