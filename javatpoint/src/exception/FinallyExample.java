89e3988a19f6017ec11fd62d05aca0672bba096a
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-21 10:46:34 +0600

    Javatpoint. Java finally example

---
+++ javatpoint/src/exception/FinallyExample.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class FinallyExample {
+    public static void main(String[] args) {
+        try {
+            int x = 300;
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("finally block is executed");
+        }
+    }
+}
