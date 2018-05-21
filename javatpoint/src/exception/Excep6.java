e9464452d2c947919522636c7cddeacef9ae9052
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-21 09:39:34 +0600

    Javatpoint. Java nested try example

---
+++ javatpoint/src/exception/Excep6.java
@@ -0,0 +1,24 @@
+package exception;
+
+public class Excep6 {
+    public static void main(String[] args) {
+        try {
+            try {
+                System.out.println("going to divide");
+                int b = 39 / 0;
+            } catch (ArithmeticException e) {
+                System.out.println(e);
+            }
+            try {
+                int a[] = new int[5];
+                a[5] = 4;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
+            }
+            System.out.println("Other statement");
+        } catch (Exception e) {
+            System.out.println("handled");
+        }
+        System.out.println("normal flow...");
+    }
+}
