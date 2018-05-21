62259b712a6c2aa54294be0ebf15865ce770b746
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-21 10:23:44 +0600

    Javatpoint. Case1:You caught the exception i.e. handle the exception using try/catch.

--- javatpoint/src/exception/Testthrows2.java
+++ javatpoint/src/exception/Testthrows2.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 class M {
     void method() throws IOException {
-        throw new IOException();
+        throw new IOException("device error"); //checked exception
     }
 }
 
