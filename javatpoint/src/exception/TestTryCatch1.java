fef949a37c4f17e85a6a809488e24bb66f241956
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-21 09:08:45 +0600

    Javatpoint. Problem without exception handling

---
+++ javatpoint/src/exception/TestTryCatch1.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class TestTryCatch1 {
+    public static void main(String[] args) {
+        int data = 50 / 0;
+        System.out.println("rest of the code");
+    }
+}
