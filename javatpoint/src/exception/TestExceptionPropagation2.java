9000a91b036b71285a05b7fb104af6e0e9303a05
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-21 10:16:05 +0600

    Javatpoint. By default, Checked Exceptions are not forwarded in calling chain (propagated).

---
+++ javatpoint/src/exception/TestExceptionPropagation2.java
@@ -0,0 +1,27 @@
+package exception;
+
+import java.io.IOException;
+
+public class TestExceptionPropagation2 {
+    void m() throws IOException {
+        throw new IOException("device error");
+    }
+
+    void n() {
+        //m();
+    }
+
+    void p() {
+        try {
+            n();
+        } catch (Exception e) {
+            System.out.println("exception handled");
+        }
+    }
+
+    public static void main(String[] args) {
+        TestExceptionPropagation2 obj = new TestExceptionPropagation2();
+        obj.p();
+        System.out.println("normal flow");
+    }
+}
