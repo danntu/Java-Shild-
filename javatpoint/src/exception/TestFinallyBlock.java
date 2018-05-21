a113819845c04e1fe8e91d3cbb2f1654df39fc3f
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-21 09:59:00 +0600

    Javatpoint. Let's see the java finally example where exception occurs and not handled.

--- javatpoint/src/exception/TestFinallyBlock.java
+++ javatpoint/src/exception/TestFinallyBlock.java
@@ -13,3 +13,17 @@ public class TestFinallyBlock {
         System.out.println("rest of the code");
     }
 }
+
+class TestFinallyBlock1 {
+    public static void main(String[] args) {
+        try {
+            int data = 25 / 0;
+            System.out.println(data);
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("finally block is always executed");
+        }
+        System.out.println("rest of the code...");
+    }
+}
