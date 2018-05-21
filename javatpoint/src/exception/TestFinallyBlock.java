efb92cfef8bbfff4d42f49b36fd2f1c94cd5b298
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-21 10:01:11 +0600

    Javatpoint. Let's see the java finally example where exception occurs and handled.

--- javatpoint/src/exception/TestFinallyBlock.java
+++ javatpoint/src/exception/TestFinallyBlock.java
@@ -27,3 +27,17 @@ class TestFinallyBlock1 {
         System.out.println("rest of the code...");
     }
 }
+
+class TestFinallyBlock2 {
+    public static void main(String[] args) {
+        try {
+            int data = 25 / 0;
+            System.out.println(data);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("finally block is always executed");
+        }
+        System.out.println("rest of the code");
+    }
+}
