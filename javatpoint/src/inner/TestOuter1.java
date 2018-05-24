6ba24de18d687632f7ff53a031dcd372a6a3fb52
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-24 09:54:45 +0600

    Javatpoint. Java static nested class example with instance method

---
+++ javatpoint/src/inner/TestOuter1.java
@@ -0,0 +1,16 @@
+package inner;
+
+public class TestOuter1 {
+    static int data = 30;
+
+    static class Inner {
+        void msg() {
+            System.out.println("data is " + data);
+        }
+    }
+
+    public static void main(String[] args) {
+        TestOuter1.Inner inner = new TestOuter1.Inner();
+        inner.msg();
+    }
+}
