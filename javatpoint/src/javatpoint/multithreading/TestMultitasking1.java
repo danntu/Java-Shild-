5cac528be4ede6d9cdcf2cfb77b0e8118ad4db19
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-25 16:47:49 +0600

    Javatpoint. How to perform single task by multiple threads?

---
+++ javatpoint/src/javatpoint/multithreading/TestMultitasking1.java
@@ -0,0 +1,21 @@
+package javatpoint.multithreading;
+
+/**
+ * Created by mdaniyar on 5/25/18.
+ */
+public class TestMultitasking1 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("task one");
+    }
+
+    public static void main(String[] args) {
+        TestMultitasking1 t1 = new TestMultitasking1();
+        TestMultitasking1 t2 = new TestMultitasking1();
+        TestMultitasking1 t3 = new TestMultitasking1();
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
