89bfc066403c45a6ea456d5fd062e8b2ce5fe218
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-28 09:03:17 +0600

    Javatpoint. Java Runtime availableProcessors()

---
+++ javatpoint/src/javatpoint/multithreading/Runtime4.java
@@ -0,0 +1,10 @@
+package javatpoint.multithreading;
+
+/**
+ * Created by mdaniyar on 5/28/18.
+ */
+public class Runtime4 {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
