8dfc99fb227285f814535c191e0fa77974fa6f2c
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-28 16:49:35 +0600

    Javatpoint. Example of java PrintStream class

---
+++ javatpoint/src/javatpoint/io/PrintStreamTest.java
@@ -0,0 +1,22 @@
+package javatpoint.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+/**
+ * Created by mdaniyar on 5/28/18.
+ */
+public class PrintStreamTest {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fout = new FileOutputStream("testout.txt");
+        PrintStream printStream = new PrintStream(fout);
+        printStream.println(2018);
+        printStream.println("Hello Java");
+        printStream.println("Welcome to Java");
+        printStream.close();
+        fout.close();
+        System.out.println("Success?");
+    }
+}
