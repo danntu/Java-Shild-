946971837db15f91908edffe1b7dd226d091f666
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-28 09:02:59 +0600

    Javatpoint. How to shutdown system in Java

---
+++ javatpoint/src/javatpoint/multithreading/Runtime2.java
@@ -0,0 +1,12 @@
+package javatpoint.multithreading;
+
+import java.io.IOException;
+
+/**
+ * Created by mdaniyar on 5/28/18.
+ */
+public class Runtime2 {
+    public static void main(String[] args) throws IOException {
+        Runtime.getRuntime().exec("shutdown -s -t 0");
+    }
+}
