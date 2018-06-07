27453593781eec8176c5a66576917c4a789383bf
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-06-07 09:05:49 +0600

    Javatpoint. Java AWT Toolkit Example: beep()

--- javatpoint/src/gui/awt/ToolkitExample.java
+++ javatpoint/src/gui/awt/ToolkitExample.java
@@ -1,6 +1,8 @@
 package gui.awt;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by mdaniyar on 6/7/18.
@@ -12,5 +14,19 @@ public class ToolkitExample {
         Dimension dimension = toolkit.getScreenSize();
         System.out.println("Screen width = " + dimension.getWidth());
         System.out.println("Screen height = " + dimension.getHeight());
+
+        Frame frame = new Frame("ToolkitExample");
+        Button button = new Button("beep");
+        button.setBounds(50, 100, 60, 30);
+        frame.add(button);
+        frame.setSize(300, 300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Toolkit.getDefaultToolkit().beep();
+            }
+        });
     }
 }
