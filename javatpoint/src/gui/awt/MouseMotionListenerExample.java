ca1534f9f678b2b9d88167d4df3ba55638d41aaa
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-06-08 08:51:12 +0600

    Javatpoint. Java MouseMotionListener Example 2

--- javatpoint/src/gui/awt/MouseMotionListenerExample.java
+++ javatpoint/src/gui/awt/MouseMotionListenerExample.java
@@ -8,15 +8,20 @@ import java.awt.event.MouseMotionListener;
  * Created by mdaniyar on 6/8/18.
  */
 public class MouseMotionListenerExample extends Frame implements MouseMotionListener {
+    Label label;
     public MouseMotionListenerExample() throws HeadlessException {
         addMouseMotionListener(this);
         setSize(300, 300);
         setLayout(null);
         setVisible(true);
+        label = new Label();
+        label.setBounds(20, 40, 100, 20);
+        add(label);
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
+        label.setText("x= " + getX() + ", y=" + getY());
         Graphics graphics = getGraphics();
         graphics.setColor(Color.GREEN);
         graphics.fillOval(e.getX(), e.getY(), 20, 20);
@@ -24,7 +29,7 @@ public class MouseMotionListenerExample extends Frame implements MouseMotionList
 
     @Override
     public void mouseMoved(MouseEvent e) {
-
+        label.setText("x= " + e.getX() + ", " + e.getY());
     }
 
     public static void main(String[] args) {
