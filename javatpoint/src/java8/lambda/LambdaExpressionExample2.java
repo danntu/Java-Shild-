c7f1a0b4f8309c1c4c10a2c446f8d0f01f1967b4
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-21 21:30:11 +0600

    Javatpoint. Java 8. Java Lambda Expression Example

---
+++ javatpoint/src/java8/lambda/LambdaExpressionExample2.java
@@ -0,0 +1,15 @@
+package java8.lambda;
+
+interface Drawable2 {
+    void draw();
+}
+
+public class LambdaExpressionExample2 {
+    public static void main(String[] args) {
+        int width = 100;
+        Drawable2 drawable2 = () -> {
+            System.out.println("Drawing 2 = " + width);
+        };
+        drawable2.draw();
+    }
+}
