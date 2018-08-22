9a5579bec74fb6bcf84a6848097d623486972fcc
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-22 11:05:37 +0600

    Javatpoint. Java 8. Reference to a Static Method. we are using predefined functional interface Runnable to refer static method.

---
+++ javatpoint/src/java8/methodreference/MethodReference2.java
@@ -0,0 +1,12 @@
+package java8.methodreference;
+
+public class MethodReference2 {
+    public static void ThreadStatus() {
+        System.out.println("Thread is running");
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(MethodReference2::ThreadStatus);
+        thread.start();
+    }
+}
