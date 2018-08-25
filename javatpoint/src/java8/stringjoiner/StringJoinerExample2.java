532fe0958d4e25cbf89efc195e3753854ea9db9a
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-25 12:40:23 +0600

    Javatpoint. Java 8. StringJoiner. Java StringJoiner Example: adding prefix and suffix

---
+++ javatpoint/src/java8/stringjoiner/StringJoinerExample2.java
@@ -0,0 +1,14 @@
+package java8.stringjoiner;
+
+import java.util.StringJoiner;
+
+public class StringJoinerExample2 {
+    public static void main(String[] args) {
+        StringJoiner joiner = new StringJoiner(";", "[", "]");
+        joiner.add("Myrzakanov");
+        joiner.add("Daniyar");
+        joiner.add("Usenovich");
+
+        System.out.println(joiner);
+    }
+}
