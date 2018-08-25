926277b8b7d84a1b7e2bb04af3779734c642ee51
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-25 12:23:19 +0600

    Javatpoint. Java 8. Collectors. Java Collectors Example: Converting Data as a Set

--- javatpoint/src/java8/collectors/CollectorsExample2.java
+++ javatpoint/src/java8/collectors/CollectorsExample2.java
@@ -13,7 +13,7 @@ public class CollectorsExample2 {
 
         productList.add(new Product(1, "HP Laptop", 25000f));
         productList.add(new Product(2, "Dell Laptop", 25000f));
-        productList.add(new Product(3, "Lenevo Laptop", 28000f));
+        productList.add(new Product(3, "Lenovo Laptop", 28000f));
         productList.add(new Product(4, "Sony Laptop", 28000f));
         productList.add(new Product(5, "Apple Laptop", 90000f));
 
