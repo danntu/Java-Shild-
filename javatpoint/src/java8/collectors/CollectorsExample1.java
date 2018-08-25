259d6b6d9809d11fc985529e7f7148607f02a223
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-25 11:43:26 +0600

    Javatpoint. Java 8. Collectors. Java Collectors Example: Fetching data as a List

---
+++ javatpoint/src/java8/collectors/CollectorsExample1.java
@@ -0,0 +1,25 @@
+package java8.collectors;
+
+import java8.stream.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CollectorsExample1 {
+    public static void main(String[] args) {
+        List<Product> productList = new ArrayList<>();
+
+        productList.add(new Product(1, "HP Laptop", 25000f));
+        productList.add(new Product(2, "Dell Laptop", 30000f));
+        productList.add(new Product(3, "Lenevo Laptop", 28000f));
+        productList.add(new Product(4, "Sony Laptop", 28000f));
+        productList.add(new Product(5, "Apple Laptop", 90000f));
+
+        List<Float> productPriceList =
+                productList.stream()
+                        .map(product -> product.getPrice())
+                        .collect(Collectors.toList());
+        System.out.println(productPriceList);
+    }
+}
