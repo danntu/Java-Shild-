f5c3dd04a24532f8bd6f0386d02c6de409b6cf4b
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-28 09:44:40 +0600

    Javatpoint. Java 8. Optional. Example: Java Program without using Optional

---
+++ javatpoint/src/java8/optional/OptionalExample1.java
@@ -0,0 +1,9 @@
+package java8.optional;
+
+public class OptionalExample1 {
+    public static void main(String[] args) {
+        String str[] = new String[10];
+        String lowercaseString = str[5].toLowerCase();
+        System.out.println(lowercaseString);
+    }
+}
