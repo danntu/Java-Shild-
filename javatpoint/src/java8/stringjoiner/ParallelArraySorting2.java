caf99386420e63eec3dfb0d8bd215b5bfa7d2460
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-28 11:21:09 +0600

    Javatpoint. Java 8. Java Parallel Array Sorting Example: Passing Start and End Index

---
+++ javatpoint/src/java8/stringjoiner/ParallelArraySorting2.java
@@ -0,0 +1,22 @@
+package java8.stringjoiner;
+
+import java.util.Arrays;
+
+public class ParallelArraySorting2 {
+    public static void main(String[] args) {
+        int a[] = {5, 8, 1, 0, 6, 9, 50, -3};
+
+        for (int i :
+                a) {
+            System.out.print(i + " ");
+        }
+
+        Arrays.parallelSort(a, 0, 4);
+        System.out.println("\nArray elements after sorting");
+
+        for (int i :
+                a) {
+            System.out.print(i + " ");
+        }
+    }
+}
