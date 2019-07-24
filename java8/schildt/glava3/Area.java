aa60690e967164d3797c1c06980dcd05b01fddba
danik <dan_ntu@mail.ru>
2019-07-24 11:25:33 +0600

    Тип double

---
+++ java8/schildt/glava3/Area.java
@@ -0,0 +1,12 @@
+package schildt.glava3;
+
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+        r = 10.8;
+        pi = 3.1416;
+        a = pi * r *r;
+
+        System.out.println("Площадь круга равна " + a);
+    }
+}
