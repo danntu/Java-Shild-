e4fe18542d8f742a857a2410243768049d7b1956
danik <dan_ntu@mail.ru>
2019-07-24 13:57:42 +0600

    Одномерный массив

---
+++ java8/schildt/glava3/Array.java
@@ -0,0 +1,28 @@
+package schildt.glava3;
+
+public class Array {
+    public static void main(String[] args) {
+        int month_days[];
+
+        month_days = new int[12];
+        int month_days2[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+
+        month_days[0]=31;
+        month_days[1]=28;
+        month_days[2]=31;
+        month_days[3]=30;
+        month_days[4]=31;
+        month_days[5]=30;
+        month_days[6]=31;
+        month_days[7]=31;
+        month_days[8]=30;
+        month_days[9]=31;
+        month_days[10]=30;
+        month_days[11]=31;
+
+        System.out.println("В апреле " + month_days[3]+" дней.");
+        System.out.println("В апреле " + month_days2[3]+" дней.");
+
+    }
+}
